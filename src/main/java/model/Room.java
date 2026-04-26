a2b9d27ec9b763ec018a0eb869e4ce8a7a75b517
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2026-04-26 23:13:31 +0545

    Update Room.java
    wrote code in room.java

--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -8,6 +8,46 @@ package model;
  *
  * @author poema
  */
+
+import java.util.*;
+
 public class Room {
+    private String id;
+    private String name;
+    private int capacity;
+    
+    private List<String> sensorIDs= new ArrayList<>();
+    
+    public Room(){}
+    
+    public String getID(){
+        return this.id;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public int getCapacity(){
+        return this.capacity;
+    }
+    
+    public List<String> getSensorIds(){
+        return this.sensorIDs;
+    }
+    
+    
+    public void setID(String Id){
+        this.id=Id;
+    }
+    
+    public void setName(String Name){
+        this.name=Name;
+    }
+    
+    public void setCapacity(int Capacity){
+        this.capacity=Capacity;
+    }
+    
     
 }
