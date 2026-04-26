d271efe006194da79c1907bd5b495384db9fdc56
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2026-04-27 01:45:32 +0545

    Update RoomResource.java
    wrote code in RoomResource.java

--- src/main/java/resource/RoomResource.java
+++ src/main/java/resource/RoomResource.java
@@ -8,6 +8,56 @@ package resource;
  *
  * @author poema
  */
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+import model.Room;
+
+import java.util.*;
+
+
+@Path("/rooms")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class RoomResource {
+    
+    private static Map<String,Room> rooms = new HashMap<>();
+    
+    @GET
+    public Collection<Room> getAllRooms(){
+        return rooms.values();
+    }
+    
+    @POST
+    public Response createRoom(Room room){
+        rooms.put(room.getID(), room);
+        return Response.status(201).entity(room).build();
+    }
+    
+    @GET
+    @Path("/{id}")
+    public Room getRoom(@PathParam("id") String ID){
+        return rooms.get(ID);
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public Response deleteRoom(@PathParam("id") String id) {
+
+        Room room = rooms.get(id);
+
+        // block deletion if sensors exist
+        if (!room.getSensorIds().isEmpty()) {
+            throw new RuntimeException("Room not empty");
+        }
+
+        rooms.remove(id);
+        return Response.ok().build();
+    }
+
+    // allow other classes to access rooms
+    public static Map<String, Room> getRooms() {
+        return rooms;
+    }
     
 }
