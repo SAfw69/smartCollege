8850423a91d4585d8159bc2e8ab26c19b1d53348
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2026-04-27 16:00:57 +0545

    Update Sensor.java
    fixed consistency issues it has with other files

--- src/main/java/model/Sensor.java
+++ src/main/java/model/Sensor.java
@@ -11,25 +11,25 @@ package model;
  */
 public class Sensor {
     private String id;
-    private String type;
-    private String status;
+    private String type; // temp, CO2, etc
+    private String status; // Maintenance or Active
     private double currentValue;
     private String roomId;
 
     public Sensor() {}
 
-    public String getId() { return id; }
+    public String getId() { return this.id; }
     public void setId(String id) { this.id = id; }
 
-    public String getType() { return type; }
+    public String getType() { return this.type; }
     public void setType(String type) { this.type = type; }
 
-    public String getStatus() { return status; }
+    public String getStatus() { return this.status; }
     public void setStatus(String status) { this.status = status; }
 
-    public double getCurrentValue() { return currentValue; }
+    public double getCurrentValue() { return this.currentValue; }
     public void setCurrentValue(double v) { this.currentValue = v; }
 
-    public String getRoomId() { return roomId; }
+    public String getRoomId() { return this.roomId; }
     public void setRoomId(String roomId) { this.roomId = roomId; }
 }
