caf782062891b4bf6fa3cd33db0c370086779de5
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2026-04-27 15:46:36 +0545

    Update SensorResource.java
    wrote code in Sensor resource

--- src/main/java/resource/SensorResource.java
+++ src/main/java/resource/SensorResource.java
@@ -8,6 +8,56 @@ package resource;
  *
  * @author poema
  */
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+import model.*;
+
+import java.util.*;
+
+
+
+@Path("/sensors")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class SensorResource {
-    
+    private static Map<String, Sensor> sensors = new HashMap<>();
+
+    @POST
+    public Response createSensor(Sensor sensor) {
+
+        Room room = RoomResource.getRooms().get(sensor.getRoomId());
+
+        if (room == null) {
+            throw new RuntimeException("Invalid room");
+        }
+
+        sensors.put(sensor.getId(), sensor);
+        room.getSensorIds().add(sensor.getId());
+
+        return Response.status(201).entity(sensor).build();
+    }
+
+    @GET
+    public List<Sensor> getSensors(@QueryParam("type") String type) {
+
+        List<Sensor> result = new ArrayList<>();
+
+        for (Sensor s : sensors.values()) {
+            if (type == null || s.getType().equalsIgnoreCase(type)) {
+                result.add(s);
+            }
+        }
+
+        return result;
+    }
+
+    @Path("/{id}/readings")
+    public SensorReadingResource getReadings() {
+        return new SensorReadingResource();
+    }
+
+    public static Map<String, Sensor> getSensorsMap() {
+        return sensors;
+    }
 }
