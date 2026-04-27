ec2af69e77b07272062bc6df5dcd4fe0ddb0ab2c
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2026-04-27 16:06:58 +0545

    Update SensorReading.java
    wrote code in sensor reading.java

--- src/main/java/model/SensorReading.java
+++ src/main/java/model/SensorReading.java
@@ -9,5 +9,35 @@ package model;
  * @author poema
  */
 public class SensorReading {
+    private String id;
+    private long timeStamp;
+    private double value;
+    
+    public SensorReading(){}
+    
+    public String getId(){
+    return this.id;
+    }
+    
+    public long getTimeStamp(){
+    return this.timeStamp;
+    }
+    
+    public double getValue(){
+    return this.value;
+    }
+    
+    public void setID(String ID){
+    this.id=ID;
+    }
+    
+    public void setTimeStamp(long Timestamp){
+    this.timeStamp=Timestamp;
+    }
+    
+    public void setValue(double Value){
+    this.value=Value;
+    }
+    
     
 }
