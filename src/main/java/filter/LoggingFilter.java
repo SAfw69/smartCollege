842218b868f0892f0c58a7ef60b6f78fd99a2737
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2026-04-29 01:37:06 +0545

    Update LoggingFilter.java
    made logger

--- src/main/java/filter/LoggingFilter.java
+++ src/main/java/filter/LoggingFilter.java
@@ -8,6 +8,27 @@ package filter;
  *
  * @author poema
  */
-public class LoggingFilter {
+
+import jakarta.ws.rs.container.*;
+import jakarta.ws.rs.ext.Provider;
+
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter,ContainerResponseFilter {
+    
+    private static final Logger logger= Logger.getLogger("API");
+    
+    @Override
+    public void filter(ContainerRequestContext req){
+        logger.info(req.getMethod() + " " + req.getUriInfo().getPath());
+        
+    }
+    
+    @Override
+    public void filter(ContainerRequestContext req, ContainerResponseContext res){
+        logger.info("Status: "+ res.getStatus());
+        
+    }
     
 }
