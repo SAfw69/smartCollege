7fb69f787ece82fe15f57602750a668cd933646d
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2026-05-01 22:55:11 +0545

    Update Main.java
    made some more final touches after updating pom.xml to java 23 by fixing some old syntax

--- src/main/java/config/Main.java
+++ src/main/java/config/Main.java
@@ -12,22 +12,23 @@ package config;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import config.AppConfig;
+import java.io.IOException;
 import java.net.URI;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         String BASE_URI="http://localhost:8000";
-        try{
+        //try{
             HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI),new AppConfig());
             server.start();
             System.out.println("The Server has been Initialized...");
-        
+        /*
         }
         catch(Exception e)
         {
             System.out.println("The Server Failed to Initialize...");
-        }
+        }*/
         
     }
 }
